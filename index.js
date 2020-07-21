972ddbd4524005bdbe21bc1c2517fbaa85a3b314
Anubhav Tyagi <anubhavtyagi00024@gmail.com>
2020-07-22 03:47:59 +0530

    Commit -1: Create the directory structure and set-up express server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const port = 8000;
+
+const app = express();
+
+
+// // using express routers
+// app.use('/', require('./routes/index'));
+
+
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        // from now we use interpolation
+        // nothing new just like string in '' or ""
+        // but some variable(which has to be printed) also added...
+        console.log(`Error in running the server: ${err}`);
+        return;
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
