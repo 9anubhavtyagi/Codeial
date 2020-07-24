be0942ad0c7ee93261e22e73c3df5fabc3211582
Anubhav Tyagi <anubhavtyagi00024@gmail.com>
2020-07-25 05:03:47 +0530

    Commit -10(f): Displaying comments. Use nested population here.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -30,7 +30,15 @@ module.exports.home = function(req, res){
 
 
     // following code is to populate the user of each post.
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         return res.render('home',{
             title: "Codeial | Home",
             heading: "Codeial Home Wall",
