14c64148bb6a37a3c8594ab04df897a6ed6befc5
Anubhav Tyagi <anubhavtyagi00024@gmail.com>
2020-07-25 01:01:34 +0530

    Commit -10(c): Succesfully shows posts and the user who post it on Home-Wall

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,16 +1,40 @@
 // first basic response for '/' request.
-module.exports.home = function(req, res){
-    // return res.end("<h1> Express is running for Codeial! </h1>");
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
+// module.exports.home = function(req, res){
+//     return res.end("<h1> Express is running for Codeial! </h1>");
 
-    return res.render('home',
-    {
-        title: "Home of Codeial",
-        heading: "Codeial"
-    });
-};
+//     console.log(req.cookies);
+//     res.cookie('user_id', 25);
 
+// };
+
+
+// second basic response for '/' request.
 // module.exports.home2 = function(req, res){
 //     return res.end("<h1> Second, Express is running for Codeial! </h1>");
 // };
+
+
+
+const Post = require('../models/post');
+
+
+module.exports.home = function(req, res){
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         heading: "Codeial Home Wall",
+    //         posts: posts
+    //     });
+    // });
+
+
+    // following code is to populate the user of each post.
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            heading: "Codeial Home Wall",
+            posts: posts
+        });
+    });
+}
