a4cc6f385674b40232f5cb23615b7170408eeda7
Anubhav Tyagi <anubhavtyagi00024@gmail.com>
2020-07-25 14:05:54 +0530

    Commit -11(b): Create a destroy method to delete comments(authorized).

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -21,3 +21,22 @@ module.exports.create = function(req, res){
         }
     });
 }
+
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            // storing post.id so that we can delete the reference of comment,
+            // which we are gonna delete, from post's comments array.
+            let postId = comment.post;
+            comment.remove(); // delete the comment
+
+            // deleting the reference of deleted comment from post's comments array.
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
