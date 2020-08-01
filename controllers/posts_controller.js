457883ab231dcb79ea2ce16f75723156a9c29967
Anubhav Tyagi <anubhavtyagi00024@gmail.com>
2020-08-02 03:40:58 +0530

    Commit -13(a): Apply AJAX for posts using 'home_posts.js' script.

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -5,10 +5,20 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created!"
+            })
+        }
+
         req.flash('success', 'Your Post has been Published!');
         return res.redirect('back');
 
@@ -27,6 +37,15 @@ module.exports.destroy = async function(req, res){
             post.remove();
     
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post Deleted!!"
+                })
+            }
 
             req.flash('success', 'Post has been Deleted!');
             return res.redirect('back');
