e1c478cc3025f030304282e8b5da1bc52397b099
Anubhav Tyagi <anubhavtyagi00024@gmail.com>
2020-08-05 06:51:00 +0530

    Commit -18(a): Create Like Model, and set-up it's controller and router.

---
+++ models/friendship.js
@@ -0,0 +1,20 @@
+// const mongoose = require('mongoose');
+
+
+// const friendshipSchema = new mongoose.Schema({
+//     // one who send friend request
+//     from_user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//     },
+//     // to whom friend request is being sent
+//     to_user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'User'
+//     }
+//     },{
+//         timestamps: true
+// });
+
+// const Friendship = mongoose.model('Friendship', friendshipSchema);
+// module.exports = Friendship;
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,9 @@ router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comments'));
 router.use('/api', require('./api'));
+router.use('/likes', require('./likes'));
+
+
 
 // exporting the central router
 module.exports = router;
